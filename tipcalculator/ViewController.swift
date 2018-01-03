aabafa5d58ac718e96b314cde02610450c37b098
Holly Ham <hham2017@gmail.com>
2018-01-02 20:03:12 -0800

    Added split bill/tip option

--- tipcalculator/ViewController.swift
+++ tipcalculator/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var resultContainer: UIView!
+    @IBOutlet weak var splitControl: UISegmentedControl!
+    @IBOutlet weak var twoPersonLabel: UILabel!
+    @IBOutlet weak var threePersonLabel: UILabel!
+    @IBOutlet weak var fourPersonLabel: UILabel!
     
     let formatter = NumberFormatter()
     var resultIsHidden = true
@@ -63,6 +67,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
     @IBAction func calculateTip(_ sender: AnyObject) {
         
         let tipPercentages = [0.15, 0.18, 0.2]
@@ -72,11 +80,23 @@ class ViewController: UIViewController {
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
+        let calculatedVals = [total, tip]
+        let twoPersonCost = calculatedVals[splitControl.selectedSegmentIndex]/2
+        let threePersonCost = calculatedVals[splitControl.selectedSegmentIndex]/3
+        let fourPersonCost = calculatedVals[splitControl.selectedSegmentIndex]/4
+        
         // Uses correct currency sign for tip and total
         formatter.numberStyle = .currency
         formatter.maximumFractionDigits = 2
+        
+        // Main Labels
         tipLabel.text = String(format: "%@", formatter.string(from: tip as NSNumber)! )
         totalLabel.text = String(format: "%@", formatter.string(from: total as NSNumber)! )
+        
+        // Split Labels
+        twoPersonLabel.text = String(format: "%@", formatter.string(from: twoPersonCost as NSNumber)! )
+        threePersonLabel.text = String(format: "%@", formatter.string(from: threePersonCost as NSNumber)! )
+        fourPersonLabel.text = String(format: "%@", formatter.string(from: fourPersonCost as NSNumber)! )
         
         let defaults = UserDefaults.standard
         // Saves entered entered bill amount
